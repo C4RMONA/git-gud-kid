1151ab6a0fc06aac0b6d343d2d37a3c7a6d816a9
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-09-28 17:26:54 -0500

    adding model and seeder files

---
+++ server/seeders/seeds.js
@@ -0,0 +1,14 @@
+const faker = require('faker');
+
+const db = require('../config/connection');
+// this line should require the models
+
+db.once('open', async () => {
+    // create Teachers
+
+    // create students
+
+    // create parents
+
+    // create messages
+})
