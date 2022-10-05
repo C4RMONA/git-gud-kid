a1a9d44413ff99cf335597a6438e9b51f9759523
moliveros <mcoliveros1202@gmail.com>
2022-10-04 22:14:13 -0400

    again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
-const env = require(dotenv).config();
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGODB_URI || 'http://mongodb+srv://mcoliveros1202:A&m120217@cluster0.1rkri4l.mongodb.net/?retryWrites=true&w=majority', {
