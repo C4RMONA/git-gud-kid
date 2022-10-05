a2ec843390fbaf4b2af72db46a6eed05cd0ce159
moliveros <mcoliveros1202@gmail.com>
2022-10-04 22:03:29 -0400

    again

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'http://mongodb+srv://mcoliveros1202:A&m120217@cluster0.1rkri4l.mongodb.net/?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
