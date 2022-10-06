0c8a87acb5b863685e996bbbcee84fbffd300c66
Samantha Guerra <88997930+Sam-Antics@users.noreply.github.com>
2022-10-06 14:54:48 -0500

    corrections that didn't get caught in the last merge to develop

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -9,7 +9,7 @@ export const LOGIN_USER = gql`
       }
     }
   }
-`
+`;
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -19,7 +19,7 @@ export const ADD_USER = gql`
       }
     }
   }
-`
+`;
 
 export const ADD_POST = gql`
   mutation addPost($postText: String!) {
@@ -36,54 +36,4 @@ export const ADD_POST = gql`
       }
     }
   }
-`
-
-// export const ADD_PARENT = gql`
-//   mutation addParent($studentId: String!, $email: String!, $password: String!) {
-//     addParent(studentId: $studentId, email: $email, password: $password) {
-//       token
-//       parent {
-//         _id
-//         name
-//       }
-//     }
-//   }
-// `
-
-// export const LOGIN_TEACHER = gql`
-//   mutation login($email: String!, $password: String!) {
-//     login(email: $email, password: $password) {
-//       token
-//       teacher {
-//         _id
-//         name
-//       }
-//     }
-//   }
-// `
-
-// export const ADD_TEACHER = gql`
-//   mutation AddTeacher($teacherId: String!, $email: String!, $password: String!) {
-//     addTeacher(teacherId: $teacherId, email: $email, password: $password) {
-//       token
-//       teacher {
-//         _id
-//         name
-//       }
-//     }
-//   }
-// `
-
-// export const ADD_COMMENT = gql`
-//   mutation addComment($PostId: ID!, $commentText: String!) {
-//     addComment(commentId: $commentId, commentText: $commentText) {
-//       _id
-//       commentCount
-//       comments {
-//         _id
-//         commentText
-//         parent
-//       }
-//     }
-//   }
-// `
+`;
