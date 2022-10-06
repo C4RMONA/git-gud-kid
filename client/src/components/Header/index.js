2d02d2839be659e5b63cf4178e881e5efce6531c
chris <christiancarmona3@gmail.com>
2022-10-05 21:56:15 -0500

    auth rendering in place

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Box, Grid, createTheme, ThemeProvider } from '@mui/material';
 import './index.css'
 import { teal, cyan } from '@mui/material/colors';
+import Auth from '../../utils/auth'
 
 const theme = createTheme({
   palette: {
@@ -19,26 +20,39 @@ const theme = createTheme({
 
 const Header = () => {
 
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  }
+
   return (
     <ThemeProvider theme={theme}>
-    <Box sx={{ flexGrow: 1, bgcolor: 'primary.main'}}>
-      <Grid container justifyContent='space-between' alignItems="center">
-        <Grid>
-          <Link className='home' to='/'>
-            <h1 className='home' >ReachTeach</h1>
-          </Link>
+      <Box sx={{ flexGrow: 1, bgcolor: 'primary.main' }}>
+        <Grid container justifyContent='space-between' alignItems="center">
+          <Grid>
+            <Link className='home' to='/'>
+              <h1 className='home' >ReachTeach</h1>
+            </Link>
+          </Grid>
+          <Grid>
+            <nav>
+              {Auth.loggedIn() ? (
+                <>
+                  <Link className='headerNav' to='/dashboard'>Dashboard</Link> 
+                  <Link className='headerNav' onClick={logout}>Logout</Link>
+                </>
+              ) : (
+
+                <ul>
+                  <Link className='headerNav' to='/login'>Login</Link>
+                  <Link className='headerNav' to='/signup'>Signup</Link>
+                  <Link className='headerNav'>Contact Us</Link>
+                </ul>
+              )}
+            </nav>
+          </Grid>
         </Grid>
-        <Grid>
-          <nav>
-            <ul>
-              <Link className='headerNav' to='/login'>Login</Link>
-              <Link className='headerNav' to='/signup'>Signup</Link>
-              <Link className='headerNav'>Contact Us</Link>
-            </ul>
-          </nav>
-        </Grid>
-      </Grid>
-    </Box>
+      </Box>
     </ThemeProvider>
   )
 }
